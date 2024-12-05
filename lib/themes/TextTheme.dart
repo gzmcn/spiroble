43eda51638d6e4125bc6d9ddd1a23b0da0d0c6ac
gzmcn <220601117@bakircay.edu.tr>
2024-12-05 13:37:29 +0300

    theme2

--- lib/themes/TextTheme.dart
+++ lib/themes/TextTheme.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 class TTextTheme{
   TTextTheme._();
 
-  static TextTheme lightTextTheme = TextTheme();
-  static TextTheme darkTextTheme = TextTheme();
+  static TextTheme lightTextTheme = TextTheme(
+    headlineLarge: TextStyle().copyWith(fontSize: 35, fontWeight:  FontWeight.bold, color: Colors.black),
+    headlineMedium: TextStyle().copyWith(fontSize: 24, fontWeight:  FontWeight.w600, color: Colors.black),
+    headlineSmall: TextStyle().copyWith(fontSize: 18, fontWeight:  FontWeight.w300, color: Colors.black),
+  );
+  static TextTheme darkTextTheme = TextTheme(
+    headlineLarge: TextStyle().copyWith(fontSize: 35, fontWeight:  FontWeight.bold, color: Colors.white),
+    headlineMedium: TextStyle().copyWith(fontSize: 24, fontWeight:  FontWeight.w600, color: Colors.white),
+    headlineSmall: TextStyle().copyWith(fontSize: 18, fontWeight:  FontWeight.w300, color: Colors.white),
+  );
 }
 
 
