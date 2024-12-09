61d37cf4d075e02057d1878b7a0fea0977828bf4
Ulaş Uçan <uls.ucn@gmail.com>
2024-12-09 15:07:29 +0300

    push

--- lib/blocs/theme.bloc.dart
+++ lib/blocs/theme.bloc.dart
@@ -16,31 +16,32 @@ abstract class ThemeState {
 class LightThemeState extends ThemeState {
   @override
   ThemeData get themeData => ThemeData(
-    useMaterial3: true,
-    focusColor: Color(0xFF6F5191),
-    cardColor: Color(0xFF6F5191),
-    primaryColor: Colors.purple[900],
-    canvasColor: Color(0xC5C7B9F1),
-    scaffoldBackgroundColor: Color(0xFF503A93),
-    primaryColorDark: Color(0xFF916BBD),
-    tabBarTheme: TabBarTheme(
-        dividerColor: Colors.purple[900],
-    ),
-    colorScheme: ColorScheme.fromSeed(
-      brightness: Brightness.light,
-      seedColor: const Color.fromARGB(255, 82, 14, 94),
-    ),
-    textTheme: GoogleFonts.latoTextTheme(
-      ThemeData(brightness: Brightness.light).textTheme,
-    ).copyWith(
-      bodyLarge: TextStyle(
-        color: Colors.black, // Light modda siyah metin
-      ),
-      bodyMedium: TextStyle(
-        color: Colors.black, // Light modda siyah metin
-      ),
-    ),
-  );
+        useMaterial3: true,
+        focusColor: Color(0xFF6F5191),
+        cardColor: Color(0xFF6F5191),
+        primaryColor: Colors.purple[900],
+        canvasColor: Color(0xC5C7B9F1),
+        scaffoldBackgroundColor: Color(0xFF503A93),
+        secondaryHeaderColor: Colors.orange,
+        primaryColorDark: Color(0xFF916BBD),
+        tabBarTheme: TabBarTheme(
+          dividerColor: Colors.purple[900],
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.light,
+          seedColor: const Color.fromARGB(255, 82, 14, 94),
+        ),
+        textTheme: GoogleFonts.latoTextTheme(
+          ThemeData(brightness: Brightness.light).textTheme,
+        ).copyWith(
+          bodyLarge: TextStyle(
+            color: Colors.black, // Light modda siyah metin
+          ),
+          bodyMedium: TextStyle(
+            color: Colors.black, // Light modda siyah metin
+          ),
+        ),
+      );
 }
 
 class DarkThemeState extends ThemeState {
@@ -70,10 +71,7 @@ class DarkThemeState extends ThemeState {
         bodyMedium: TextStyle(
           color: Colors.white, // Dark modda beyaz metin
         ),
-
       ));
-
-
 }
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
--- lib/screens/bluetoothScreen.dart
+++ lib/screens/bluetoothScreen.dart
@@ -65,180 +65,187 @@ class _BluetoothScreenState extends State<BluetoothScreen> {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         body: Consumer<BluetoothConnectionManager>(
             builder: (context, _bleManager, child) {
-              return StreamBuilder<List<DiscoveredDevice>>(
-                  stream: _bleManager.DiscoveredDeviceStream,
-                  builder: (context, snapshot) {
-                    // sıkıntı çıkarsa bi bak
-                    final devices = snapshot.data ?? [];
+          return StreamBuilder<List<DiscoveredDevice>>(
+              stream: _bleManager.DiscoveredDeviceStream,
+              builder: (context, snapshot) {
+                // sıkıntı çıkarsa bi bak
+                final devices = snapshot.data ?? [];
 
-                    final deviceToConnect = devices.firstWhere(
-                          (device) => device.name == "Spirometer",
-                      orElse: () => DiscoveredDevice(
-                        id: "",
-                        name: "",
-                        manufacturerData: Uint8List(0),
-                        serviceData: {},
-                        rssi: 0,
-                        serviceUuids: [],
+                final deviceToConnect = devices.firstWhere(
+                  (device) => device.name == "Spirometer",
+                  orElse: () => DiscoveredDevice(
+                    id: "",
+                    name: "",
+                    manufacturerData: Uint8List(0),
+                    serviceData: {},
+                    rssi: 0,
+                    serviceUuids: [],
+                  ),
+                );
+
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    // Header Section
+                    Container(
+                      padding: EdgeInsets.only(top: 50, bottom: 20),
+                      color: Theme.of(context).primaryColor,
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Cihazınızı Bağlayın",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              "Cihaz bağlama ekranı",
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.7),
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    );
+                    ),
 
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        // Header Section
-                        Container(
-                          padding: EdgeInsets.only(top: 50, bottom: 20),
-                          color: Theme.of(context).primaryColor,
-                          child: Center(
-                            child: Column(
-                              children: [
-                                Text(
-                                  "Cihazınızı Bağlayın",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18,
-                                  ),
+                    // Spacer
+                    SizedBox(height: 30),
+
+                    // Main Content
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Illustration Section
+                          Image.asset(
+                            'assets/bluetooth.png', // Bluetooth resminin adını güncelleyin
+                            // Ensure this asset is added in pubspec.yaml
+                            width: 180,
+                            height: 180,
+                            color: Colors.orange,
+                          ),
+
+                          SizedBox(height: 20),
+
+                          // Connecting Text
+                          Column(
+                            children: [
+                              SizedBox(height: 10),
+                              Text(
+                                _bleManager.checkConnection()
+                                    ? "Bağlantı başarılı!"
+                                    : "Bağlantı yok, tekrar deneyiniz",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                SizedBox(height: 10),
-                                Text(
-                                  "Cihaz bağlama ekranı",
-                                  style: TextStyle(
-                                    color: Colors.white.withOpacity(0.7),
-                                    fontSize: 14,
-                                  ),
-                                ),
-                              ],
+                              ),
+                            ],
+                          ),
+
+                          SizedBox(height: 40),
+
+                          // Connection Permission Request
+                          Text(
+                            "Spiroble cihazı bluetooth light energy teknolojisi kullanır",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
                             ),
                           ),
-                        ),
+                        ],
+                      ),
+                    ),
 
-                        // Spacer
-                        SizedBox(height: 30),
-
-                        // Main Content
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              // Illustration Section
-                              Image.asset(
-                                'assets/bluetooth.png',  // Bluetooth resminin adını güncelleyin
-                                // Ensure this asset is added in pubspec.yaml
-                                width: 180,
-                                height: 180,
-                                color: Colors.orange,
+                    // Bottom Buttons
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment
+                            .spaceBetween, // Dikey hizalama için SpaceBetween kullanın
+                        children: [
+                          // Deny Button
+                          OutlinedButton(
+                            onPressed: _bleManager.checkConnection()
+                                ? _denyConnection
+                                : null,
+                            style: OutlinedButton.styleFrom(
+                              side: BorderSide(color: Colors.grey),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
                               ),
-
-                              SizedBox(height: 20),
-
-                              // Connecting Text
-                              Column(
-                                children: [
-                                  SizedBox(height: 10),
-                                  Text(
-                                    _bleManager.checkConnection()
-                                        ? "Bağlantı başarılı!"
-                                        : "Bağlantı yok, tekrar deneyiniz",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              
-
-                              SizedBox(height: 40),
-
-                              // Connection Permission Request
-                              Text(
-                                "Spiroble cihazı bluetooth light energy teknolojisi kullanır",
-                                textAlign: TextAlign.center,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 5, vertical: 12),
+                              child: Text(
+                                "Bağlantıyı durdur",
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 16,
                                 ),
                               ),
-                            ],
+                            ),
                           ),
-                        ),
 
-                        // Bottom Buttons
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,  // Dikey hizalama için SpaceBetween kullanın
-                            children: [
-                              // Deny Button
-                              OutlinedButton(
-                                onPressed: _bleManager.checkConnection() ? _denyConnection : null,
-                                style: OutlinedButton.styleFrom(
-                                  side: BorderSide(color: Colors.grey),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
+                          // ElevatedButton durumuna bağlı olarak göster/gizle
+                          Visibility(
+                            visible: !_bleManager
+                                .checkConnection(), // Bağlı değilse buton görünür
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                if (_bleManager.checkConnection()) {
+                                  _bleManager.isLoading.value = false;
+                                } else {
+                                  _bleManager.startScan();
+
+                                  if (deviceToConnect.id == null) {
+                                    _bleManager.isLoading.value = false;
+                                    print("Cihaz bulunamadı");
+                                  } else {
+                                    await _bleManager
+                                        .connectToDevice(deviceToConnect.id);
+                                    _bleManager.isLoading.value = false;
+                                  }
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Theme.of(context).secondaryHeaderColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
                                 ),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 12),
-                                  child: Text(
-                                    "Bağlantıyı durdur",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                    ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 12),
+                                child: Text(
+                                  _bleManager.checkConnection()
+                                      ? "Bağlantı başarılı!"
+                                      : "Bağlan",
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
                                   ),
                                 ),
                               ),
-
-                              // ElevatedButton durumuna bağlı olarak göster/gizle
-                              Visibility(
-                                visible: !_bleManager.checkConnection(), // Bağlı değilse buton görünür
-                                child: ElevatedButton(
-                                  onPressed: () async {
-                                    if (_bleManager.checkConnection()) {
-                                      _bleManager.isLoading.value = false;
-                                    } else {
-                                      _bleManager.startScan();
-
-                                      if (deviceToConnect.id == null) {
-                                        _bleManager.isLoading.value = false;
-                                        print("Cihaz bulunamadı");
-                                      } else {
-                                        await _bleManager.connectToDevice(deviceToConnect.id);
-                                        _bleManager.isLoading.value = false;
-                                      }
-                                    }
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: Theme.of(context).secondaryHeaderColor,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                                    child: Text(
-                                      _bleManager.checkConnection()
-                                          ? "Bağlantı başarılı!"
-                                          : "Bağlan",
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
-                        ),
-                      ],
-                    );
-                  });
-            }));
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              });
+        }));
   }
 }
